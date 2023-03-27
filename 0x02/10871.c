2592c006179a3537ed7ec4a5fab3d14196cb4a9a
minji <olivia09292@gmail.com>
2023-03-27 21:35:29 +0900

    first

---
+++ 0x02/10871.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    int X;
+    int array[10100];
+    int value;
+
+    scanf("%d %d\n", &N, &X);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &value);
+        array[i] = value;
+        if (array[i] < x)
+            printf("%d ", array[i]);
+    }
+    return (0);
+}

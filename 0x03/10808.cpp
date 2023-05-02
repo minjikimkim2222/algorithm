e791c5ab9eac727cdb18d37a9948a8404c3e5b15
김민지 <minji@gimminjiui-MacBookPro.local>
2023-05-02 23:18:39 +0900

    추가

--- 0x03/10808.cpp
+++ 0x03/10808.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 // 이것도 뭔가 쉬운데 왜 ?  이러지 말고, 아는 걸 최대한 단순화시켜야 풀 수 있는거임.
 int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     string s;
     int result[26] = {0};
     int i = 0;

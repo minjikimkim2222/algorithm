979dfeb24e39fecaab0ef810795f837f6a1e8d0e
김민지 <minji@gimminjiui-MacBookPro.local>
2023-05-02 23:14:35 +0900

    10808

---
+++ 0x03/10808.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+// 이것도 뭔가 쉬운데 왜 ?  이러지 말고, 아는 걸 최대한 단순화시켜야 풀 수 있는거임.
+int main(void){
+    string s;
+    int result[26] = {0};
+    int i = 0;
+
+    cin >> s;
+    while (s[i]){
+        result[s[i] - 97]++;
+        i++;
+    }
+    for (int i = 0; i < 26; i++){
+        cout << result[i] << ' ';
+    }
+    cout << "\n";
+}

6de7645b5412b5967cf1dd1d8add060dad52f0a4
김민지 <minji@gimminjiui-MacBookPro.local>
2023-06-02 14:18:12 +0900

    sort_add

--- class_DATA_STRUCTURE/14_1/bubblesort.cpp
+++ class_DATA_STRUCTURE/14_1/bubblesort.cpp
@@ -16,7 +16,7 @@ void bubbleSort(vector<int>& A)
     
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - 1; j++)
+        for (int j = 0; j < n - 1 - i; j++)
         {
             if (A[j] > A[j + 1])
                 swap(A[j], A[j + 1]);

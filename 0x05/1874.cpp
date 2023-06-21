ca44f8d7c6797e79198c173a9cb1e5c3bb6b84c2
김민지 <minji@gimminjiui-MacBookPro.local>
2023-06-21 21:37:39 +0900

    비교대상을 명확히 할 것

---
+++ 0x05/1874.cpp
@@ -0,0 +1,42 @@
+// 핵심 규칙을 발견할 것 >> "pop하는 순서임, pop을 다 하고 나면 실행이 끝남"
+// 단, 제발 specfic말고 예시 관찰하고 "일반화된 규칙"을 도출할 것!
+
+// 1. "비교"하는 애가 뭐랑 뭔지! 정확하게 "변수로" 생각 <<< 이걸 습관화!
+    // >> 비교할 수 있는 후보가 뭐랑 뭐가 있는지!
+
+// 2. string 자료형을 output으로 활용하기!
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    
+    stack<int> st;
+    int cnt = 1; // 오름차순 숫자
+    string ans;
+    while (n--)
+    {
+        int t;
+        cin >> t; // 매번 입력숫자
+        while (cnt <= t){
+            st.push(cnt);
+            cnt++;
+            ans = ans + "+\n";
+        }
+        if (st.top() != t){
+            cout << "NO\n";
+            return (0);
+        }
+        st.pop();
+        ans = ans + "-\n";
+    }
+    cout << ans;
+}

1bbe384217f157991f06a7b85cfded2f83e8aec7
김민지 <minji@gimminjiui-MacBookPro.local>
2023-08-09 13:41:49 +0900

    description edif

--- barkingDog_algo/0x0D/예제1_15683.cpp
+++ barkingDog_algo/0x0D/예제1_15683.cpp
@@ -27,7 +27,7 @@ using namespace std;
 int N, M;
 int arr[8][8];
 vector<pair<int, int>> cctv;
-int ans = 987654321;
+int ans = 9876543;
 
 int dx[4] = {0, -1, 0, 1}; // 우, 상, 좌, 하
 int dy[4] = {1, 0, -1, 0};
@@ -43,11 +43,14 @@ void check(int x, int y, int dir){
         if(arr[nx][ny] == 6) return;
         if(arr[nx][ny] != 0) continue;
         arr[nx][ny] = -1;
-    }
+    } // dir 방향으로 x좌표, y좌표를 계속 늘리되,
+      // 벽이나 범위를 벗어나면 return, 그게 아니라면 -1로 써줌.
+      // 즉, cctv가 감시했다는 의미의 arr배열 값은 -1
 }
 
+// idx번째 cctv를 회전
 void dfs(int idx){
-    if(idx == cctv.size()){
+    if(idx == cctv.size()){ // 모든 cctv 개수를 확인했을 때
         int cnt = 0;
         for(int i = 0; i < N; i++)
             for(int j = 0; j < M; j++)
@@ -90,6 +93,17 @@ void dfs(int idx){
 
         dfs(idx+1);
         // case 종료이므로 초기화
+        // 초기 상태로 복구, 회전을 수행한 후, 다시 원래 상태로 복구.!
+        // why? 
+        /*
+        예를 들어, cctv 1번과 2번이 있다고 하자.
+        일단 cctv 1번(dir = 0)과 cctv 2번(dir = 0)일 때의 결과를 반영한 arr가 되고,
+        if문 재귀 종료조건에 걸려 return으로 dfs(1)의 실행 다음으로 여기로 돌아오겠지.
+        그러면 이제 cctv 1번 (dir = 0)과 cctv 2번 (dir = 1)일 때만 관찰하고 싶으니,
+        기존에 tmp에는 cctv 1번(dir = 0)일 때가 적혀있고, arr에 있던 추가적인 cctv 2번(dir = 0)인 결과는 아직 없으므로,
+        arr를 tmp로 되돌리고,
+        dfs(1)에서 for문으로 dir = 1을 탐색한다!
+        */
         for(int i = 0; i < N; i++)
             for(int j = 0; j < M; j++)
                 arr[i][j] = tmp[i][j];

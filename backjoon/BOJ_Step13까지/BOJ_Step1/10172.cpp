bd75826ca8c5f3c40ab9c44167e4fedb5e0c810a
김민지 <olivia0929@naver.com>
2023-11-04 16:54:48 +0900

    step 01 complete

--- backjoon/BOJ_Step13까지/BOJ_Step1/10172.cpp
+++ backjoon/BOJ_Step13까지/BOJ_Step1/10172.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 int main(void)
 {
-    cout << "|\\_/|" << '\n';
-    cout << "|q p|   /}" << '\n';
-    cout << "( 0 )\"\"\"\\" << '\n';
-    cout << "|\"^\"`    |" << '\n';
-    cout << "||_/=\\__|" ;
+    cout << "|\\_/|\n";
+    cout << "|q p|   /}\n";
+    cout << "( 0 )\"\"\"\\\n";
+    cout << "|\"^\"`    |\n";
+    cout << "||_/=\\\\__|\n" ;
+    return (0);
 
 }
